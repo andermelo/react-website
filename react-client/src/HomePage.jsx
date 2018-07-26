027c3322829b8f162f294cb337218e3ab67e6e78
Anderson Melo <anderson.90@gmail.com>
2018-07-26 17:01:17 -0300

    cont...

---
+++ react-client/src/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Componet {
+  render() {
+    return (
+      <div>
+       Home page
+      </div>
+    )
+  }
+}
+
+export default HomePage;
